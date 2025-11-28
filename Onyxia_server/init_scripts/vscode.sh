fe742095b2352074ea095cc68de6f44f12497707
Edouard Pignède <edouard.pignede@gmail.com>
2025-11-28 17:56:44 +0100

    Uncomment and enable git clone in vscode.sh

--- Onyxia_server/init_scripts/vscode.sh
+++ Onyxia_server/init_scripts/vscode.sh
@@ -6,6 +6,7 @@
 # - change VSCode main settings
 # - install main VSCode extensions 
 
+# see example of init scripts here: https://github.com/InseeFrLab/sspcloud-init-scripts/tree/main/vscode
 
 # Parses the argument from the onyxia init 
 FULL_NAME="$1" # eg. "BETSAKA/training"
@@ -13,20 +14,17 @@ SERV_FOLD="$2"
 PROJ_NAME="${FULL_NAME##*/}" # then "training"
 # Creation of automatic variables
 WORK_DIR=/home/onyxia/work/${PROJ_NAME} # then "/home/onyxia/work/training"
-# REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/${FULL_NAME}.git # then "github.com/BETSAKA/training"
-# # Open VSCode in folder repo
-# AS_CODE_SERVER_ROOT="true"
+REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/${FULL_NAME}.git # then "github.com/BETSAKA/training"
 
-# # Clone git repo
-# git clone $REPO_URL $WORK_DIR
-# chown -R onyxia:users $WORK_DIR
+# Clone git repo
+git clone $REPO_URL $WORK_DIR
+chown -R onyxia:users $WORK_DIR
 
 # Copy files from s3
 mc cp -r s3/${SERV_FOLD}/${PROJ_NAME} /home/onyxia/work/
 chown -R onyxia:users $WORK_DIR # make sure users have rights to edit
 
 # Set vscode settings
-# Path to the VSCode settings.json file
 # Path to the VSCode settings.json file
 SETTINGS_FILE="${HOME}/.local/share/code-server/User/settings.json"
 
