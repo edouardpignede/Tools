3b9f5ee63271f6da55abebecd717a091165e454d
Edouard Pignède <edouard.pignede@gmail.com>
2025-12-03 10:47:06 +0100

    Set up Python virtual environment in vscode.sh

--- Onyxia_server/init_scripts/vscode.sh
+++ Onyxia_server/init_scripts/vscode.sh
@@ -84,8 +84,12 @@ code-server --install-extension copilot.vsix
 # code-server --install-extension copilot-chat.vsix
 rm copilot.vsix #copilot-chat.vsix
 
-# Install python packages
+# Set up python .venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
 
+# Install python packages
 # Install additional packages if a requirements.txt file is present in the project
 REQUIREMENTS_FILE=${WORK_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
